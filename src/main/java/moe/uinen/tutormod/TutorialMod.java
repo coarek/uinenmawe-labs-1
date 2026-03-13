46416bd132a138def91e34f46ba075be99a8cc07
Roman Awksentjew <rawksentjew@AKTuBHbIurOMec.local>
2026-03-14 00:59:40 +0300

    alalalalala

---
+++ src/main/java/moe/uinen/tutormod/TutorialMod.java
@@ -0,0 +1,16 @@
+package moe.uinen.tutormod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "Uinen's first mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
